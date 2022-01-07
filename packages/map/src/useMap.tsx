7d98ed3b3cfb024e1ce42875c4d5b1b3f4c9a50a
chenzhaozheng <chenzhaozheng@LAPTOP-GK4GS2RH.localdomain>
2022-01-07 15:52:04 +0800

    test: package

--- packages/map/src/useMap.tsx
+++ packages/map/src/useMap.tsx
@@ -30,39 +30,6 @@ export const useMap = (props: Partial<UseMap> = {}) => {
                 ...other,
             });
             instance = new TMap.Map(container, { zoom, ...other })
-            // const center = [113.23, 23.16];
-            // var center2 = new TMap.LatLng(center[1], center[0]);
-            // instance = new TMap.Map(container, {
-            //     zoom,//设置地图缩放级别
-            //     center: center2,//设置地图中心点坐标
-            //     mapStyleId: 'style1',//个性化样式
-            //     baseMap: {
-            //         type: "vector",
-            //         features: ["base", "point"], // 隐藏矢量文字
-            //     },
-            //     minZoom: 16,
-            //     maxZoom: 20,
-            //     toggleAnimation: false,
-            //     doubleClickZoom: false,
-            //     viewMode: '2D',
-            //     showControl: false,
-            //     businessSelectText: '',
-            //     // zoomControl: false,
-            //     // mapTypeControl: false
-            // });
-            // instance.setViewMode('2D');
-            // if (!instance.getControl(TMap.constants.DEFAULT_CONTROL_ID.ZOOM)) { // 如果map上不存在该控件则直接返回
-            //     return;
-            // }
-            // instance.removeControl(TMap.constants.DEFAULT_CONTROL_ID.ZOOM);
-            // instance.on('zoom_changed', Dobound.use(() => {
-            //     console.log('zoom_changed');
-            //     setZoom(zoom);
-            // }));
-            // instance.on('zoom_changed', () => {
-            //     console.log('zoom_changed');
-            //     setZoom(zoom);
-            // });
             setMap(instance);
         }
         return () => {
